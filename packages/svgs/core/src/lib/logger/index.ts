7222ec543c3e3920de263705e81bbebb6b944e32
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-11-20 13:15:55 +0900

    minor fix

--- packages/svgs/core/src/lib/logger/index.ts
+++ packages/svgs/core/src/lib/logger/index.ts
@@ -1,19 +1,24 @@
 import chalk from "chalk";
 
 function info(message: string): void {
-  console.log(chalk.cyan("[INFO]"), message);
+  console.log(chalk.cyan(message));
 }
 
 function warn(message: string): void {
-  console.warn(chalk.yellow("[WARN]"), message);
+  console.warn("⚠️", chalk.yellow(message));
 }
 
 function error(message: string): void {
-  console.error(chalk.red("[ERROR]"), message);
+  console.error("❌", chalk.red(message));
+}
+
+function success(message: string): void {
+  console.log("✅", chalk.green(message));
 }
 
 export const logger = {
   info,
   warn,
   error,
+  success,
 };
