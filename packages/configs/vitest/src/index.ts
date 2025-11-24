19b597072e8fd98050c5a8f7ad5c95ac2f5c8e8a
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2025-11-24 14:50:41 +0900

    add test packages, test configs and logger test codes

---
+++ packages/configs/vitest/src/index.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vitest/config";
+
+export const sharedConfig = defineConfig({
+  test: {
+    globals: true,
+    environment: "node",
+    coverage: {
+      provider: "istanbul",
+      reporter: [["text", { skipFull: true }]],
+    },
+  },
+});
