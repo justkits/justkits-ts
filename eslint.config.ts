56bee5f93d8921a129cd2faf0d01afab74ada10f
정승원 <vietman2@jeongseung-won-ui-Macmini.local>
2025-12-02 21:56:24 +0900

    add root level tsconfig

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,5 +1,10 @@
+import { fileURLToPath } from "node:url";
+import { dirname } from "node:path";
 import { defineConfig } from "eslint/config";
 import { allConfig } from "@justkits/eslint";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig([
   ...allConfig,
