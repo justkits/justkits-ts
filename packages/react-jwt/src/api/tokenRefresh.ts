8f2e49fb11e8c01edd537d2bef7f7f68ed7f6224
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-02-27 20:12:15 +0900

    bugfix: prevent infinite loops of broadcasting logout events

--- packages/react-jwt/src/api/tokenRefresh.ts
+++ packages/react-jwt/src/api/tokenRefresh.ts
@@ -3,10 +3,11 @@ import { AxiosInstance } from "axios";
 import { getRefreshConfig } from "../configs/state";
 import { clearAuthHeader, setAuthHeader } from "../lib/axios";
 import { broadcast } from "../lib/broadcast-channel";
-import { updateSession } from "../lib/session";
+import { getSnapshot, updateSession } from "../lib/session";
 
 export async function tokenRefreshAPI(instance: AxiosInstance) {
   const config = getRefreshConfig();
+  const { isAuthenticated } = getSnapshot();
 
   try {
     const res = await instance.post(config.endpoint);
@@ -18,8 +19,10 @@ export async function tokenRefreshAPI(instance: AxiosInstance) {
     // Defensive Programming: 그 어떤 오류라도, 로그아웃 처리를 하여 보안을 유지한다
     // 초기 로드에 실패하더라도 다른 탭에 로그인 되어있는 세션도 전부 로그아웃 처리한다
     clearAuthHeader(instance);
-    broadcast("LOGOUT");
     updateSession(false);
+    if (isAuthenticated) {
+      broadcast("LOGOUT");
+    }
     throw error; // 에러를 다시 던져서 호출한 곳에서도 인지할 수 있도록 함
   }
 }
